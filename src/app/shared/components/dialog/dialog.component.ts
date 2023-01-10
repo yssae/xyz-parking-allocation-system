e1b98f26dad6feb64f2ce798dd3435dfd228fbdc
alyssaelesterio <aklelesterio@gmail.com>
2023-01-10 14:26:09 +0800

    generic dialog added

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DEFAULT_MSG } from '../../constants/default-msg.const';
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
+  title: string = 'Success';
+  message: string = '';
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any) {
+      this.title = this.data.type;
+    }
 
   ngOnInit(): void {
+    this.setDialogTitle();
+    this.setMessage();
   }
+
+  setMessage() {
+    if(this.data.message) {
+      this.message = this.data.message;
+    }
+    else
+      this.checkDialogType() ? this.message = DEFAULT_MSG.SUCCESS : this.message = DEFAULT_MSG.ERROR;
+  }
+
+  setDialogTitle() {
+    this.data?.title ? this.title = this.data.title : this.title = this.data.type;;
+  }
+
+  checkDialogType(): boolean {
+    return this.data.type.toLowerCase() == 'success';
+  }
+
+
 
 }
---
+++ src/app/shared/constants/default-msg.const.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_MSG = {
+  SUCCESS: "Congratulations! Your request has been completed successfully. Thank you for using our service.",
+  ERROR: "An error has occurred. We apologize for the inconvenience. Our team has been notified and is working on a solution. Please try again later."
+}
